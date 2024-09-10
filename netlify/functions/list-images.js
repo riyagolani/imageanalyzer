43047caf10c5bfd93d2419eb1b4c19e887acc5b1
riyagolani <riya.s.golani@gmail.com>
2024-09-10 12:49:18 -0400

    updates environment variables

--- netlify/functions/list-images.js
+++ netlify/functions/list-images.js
@@ -20,9 +20,9 @@ exports.handler = async (event, context) => {
   try {
     const s3 = new AWS.S3();
     AWS.config.update({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: process.env.AWS_REGION
+      accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+      region: process.env.MY_AWS_REGION
     });
 
     const params = {
